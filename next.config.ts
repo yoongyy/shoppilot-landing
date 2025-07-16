812048001a074252919a1067e75d3546ff8bd698
yoongyy <yoongyy@gmail.com>
2025-07-16 10:52:44 +0800

    d

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  skipTrailingSlashRedirect: true,  // disable automatic slash redirects
   eslint: {
     ignoreDuringBuilds: true, 
   },
